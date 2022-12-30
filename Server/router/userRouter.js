c6ad00a54eb43e98938309821fea1424d12a3d01
Mohd-Imad <mohdimd123@gmail.com>
2022-12-30 22:05:29 +0530

    ok

--- Server/router/userRouter.js
+++ Server/router/userRouter.js
@@ -64,9 +64,12 @@ router.get('/:id', async (req, resp)=>{
         if(user){
             resp.status(200).json(user)
         }
+        else{
+            resp.status(401).json({msg : "No User found...!"})
+        }
     }
     catch(e){
-        resp.status(500).json({msg : "No User found...!"})
+        resp.status(500).json({msg : "Server issue...!"})
     }
 })
 
@@ -76,12 +79,61 @@ router.get('/:id', async (req, resp)=>{
     Fields:id
 */
 router.delete('/del/:id', async (req, resp)=>{
-    let userID = req.params.id
     try{
+        let userID = req.params.id
+        let user = await User.findById(userID)
+            if(!user){
+                resp.status(401).json({msg : "User not found...!"})
+            }
 
+         user = await User.findByIdAndDelete(userID)
+        resp.status(200).json({
+            result : "User deleted successfully..!",
+            user : user
+        })
+        console.log(`Deleted User : ${user}`);
+        
     }
     catch(e){
-        
+        resp.status(501).json({msg : "Server issue...!"})
+    }
+})
+
+/*
+    URL:http://127.10.99.88:8002/users/update/:id
+    Method: PUT
+    Fields:id
+*/
+router.put('/update/:id', async (req, resp)=>{
+    try{
+        let updatedUser = {
+            name : req.body.name,
+            mobile : req.body.mobile,
+            email : req.body.email,
+            password : req.body.password,
+            re_password : req.body.re_password,
+        }
+         let userID = req.params.id
+
+         let user = await User.findOne({email : updatedUser.email})
+         if(user){
+            resp.status(401).json({msg : "User with this email already exists...!"})
+         }
+
+        user = await User.findById(userID)
+         if(!user){
+            resp.status(401).json({msg :"User not found...!"})
+         }
+         user = await User.findByIdAndUpdate(userID, {$set : updatedUser})
+         resp.status(200).json({
+            result : "User details updated successfully...!",
+            user : user
+         })
+         console.log(`Updated User : ${user}`);
+         
+    }
+    catch(err){
+
     }
 })
 
