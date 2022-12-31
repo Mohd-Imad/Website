6ef7a31223a3f8cc0f7f5736f64c55a3dd1a0324
Mohd-Imad <mohdimd123@gmail.com>
2022-12-31 10:42:59 +0530

    ok

--- Client/src/Navbar/Navbar.jsx
+++ Client/src/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar = () => {
   return <>
-    <nav className="navbar navbar-dark bg-dark text-white navbar-expand-lg">
+    <nav className="navbar">
         <Link className="navbar-brand">React Form Validations</Link>
-        <ul className="navbar-nav ml-auto">
+        <ul className="navbar-ul">
             <li className="nav-list"><Link to='/registration' className="nav-link">Registration Form</Link></li>
             <li className="nav-list"><Link to='/login' className="nav-link">Login Form</Link></li>
         </ul>
