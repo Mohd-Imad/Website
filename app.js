6512a4d9b6e0d6a89d069181dda8f1594c31d239
Mohd-Imad <mohdimd123@gmail.com>
2022-12-31 16:03:23 +0530

    ok

--- app.js
+++ app.js
@@ -9,16 +9,39 @@ import mongoose from 'mongoose'
 const app = express()
 
 dotenv.config({path:'./Config/.env'})
-let port = process.env.port
-let hostname = process.env.HOST_NAME
 
+app.use(morgan('tiny'))
+app.use(cors())
+
+//to read form data
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 
 //routers
+
 app.get('/',(req,resp)=>{
     resp.send("<h1>Express Product CRUD</h1>")
 })
+app.use('/products',productRouter)
+app.use('/users',userRouter)
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://${hostname}:${port}`);
+let mongo_url = process.env.MONGO_URL
+mongoose.set('strictQuery', false)    
+mongoose.connect(mongo_url,{
+    // useNewUrlParser : false,
+    // useUnifiedTopology : false,
+}).then((resp)=>{
+    console.log("Mongo Cloud connected Successfully....!");
+})
+.catch((err)=>{
+    console.log(err);
+    process.exit(1)
+})
+
+let port = process.env.PORT
+let host = process.env.HOST_NAME
+app.listen(port,(err)=>{
+    if(err) throw err
+    console.log(`Server is running on http://${host}:${port}`);
     // console.log(`Server is running on http://localhost:${port}`);
 })
