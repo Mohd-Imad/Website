ebf6b565bc2d191e66a7614f999abc985d940103
Mohd-Imad <mohdimd123@gmail.com>
2022-12-31 15:48:39 +0530

    test-3

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+import productRouter from './router/productRouter.js'
+import userRouter from './router/userRouter.js'
+import mongoose from 'mongoose'
 
 const app = express()
 
-app.get('/', (req, resp)=>{
-    resp.status(200).send('<h1>!........Server testing........!</h1>')
+dotenv.config({path:'./Config/.env'})
+
+app.use(morgan('tiny'))
+app.use(cors())
+
+//to read form data
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+//routers
+
+app.get('/',(req,resp)=>{
+    resp.send("<h1>Express Product CRUD</h1>")
+})
+app.use('/products',productRouter)
+app.use('/users',userRouter)
+
+let mongo_url = process.env.MONGO_URL
+mongoose.set('strictQuery', false)    
+mongoose.connect(mongo_url,{
+    // useNewUrlParser : false,
+    // useUnifiedTopology : false,
+}).then((resp)=>{
+    console.log("Mongo Cloud connected Successfully....!");
+})
+.catch((err)=>{
+    console.log(err);
+    process.exit(1)
 })
 
-app.listen(8080, ()=>{
-    console.log(`Server is running on http://localhost:8080`);
+let port = process.env.PORT
+let host = process.env.HOST_NAME
+app.listen(port,host,(err)=>{
+    if(err) throw err
+    console.log(`Server is running on http://${host}:${port}`);
+    // console.log(`Server is running on http://localhost:${port}`);
 })
