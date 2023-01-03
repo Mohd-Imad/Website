afeb7f1f19d224b69f81bb2635dcbf34b64cbdc0
Mohd-Imad <mohdimd123@gmail.com>
2023-01-03 12:20:57 +0530

    ok

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,10 +6,11 @@ import Home from './Pages/HomePage/Home'
 import AboutUs from './Pages/AboutPage/AboutUs'
 import ProductsPage from './Pages/ProductPage/ProductsPage'
 //Components
-import CreateProduct from './Components/Products/CreateProduct'
-import ProductList from './Components/Products/ProductList'
-import ProductAdmin from './Components/Products/ProductAdmin'
-import ProductUpdate from './Components/Products/ProductUpdate'
+import CreateProduct from './Pages/ProductPage/Components/CreateProduct'
+import ProductList from './Pages/ProductPage/Components/ProductList'
+import ProductAdmin from './Pages/ProductPage/Components/ProductAdmin'
+import ProductUpdate from './Pages/ProductPage/Components/ProductUpdate'
+import RegisterPage from './Pages/RegisterPage/RegisterPage'
 
 const App = () => {
   return (
@@ -17,9 +18,12 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
+          {/* Pages */}
           <Route path='/home' element={<Home />} />
           <Route path='/about' element={<AboutUs />} />
           <Route path='/products' element={<ProductsPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          {/* Components */}
           <Route path='/createproduct' element={<CreateProduct />} />
           <Route path='/listproduct' element={<ProductList />} />
           <Route path='/adminproduct' element={<ProductAdmin />} />
---
+++ Client/src/Pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RegForm from './Components/Form/Register/RegForm'
+
+const RegisterPage = () => {
+  return (
+    <>
+      <h1 className='text-white'>RegisterPage</h1>
+      <RegForm />
+    </>
+  )
+}
+
+export default RegisterPage
