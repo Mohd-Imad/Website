c6842af2a7a379f1e5be0696e30c5e64efae2bdb
Mohd-Imad <mohdimd123@gmail.com>
2023-01-19 23:08:48 +0530

    ok

--- Client/src/Pages/ProductPage/Components/Product List/ProductList.jsx
+++ Client/src/Pages/ProductPage/Components/Product List/ProductList.jsx
@@ -20,8 +20,8 @@ const ProductList = () => {
 
   return (
     <>
-      <h1 className="text-white text-center mt-mi">Products</h1>
-      <div className="container">
+      {/* <h1 className="text-white text-center mt-mi">Products</h1> */}
+      <div className="container mt-mi">
         {/* <pre className='text-white'>{JSON.stringify(products)}</pre> */}
         <div className="row">
           {
@@ -30,9 +30,9 @@ const ProductList = () => {
                 products.map((product) => {
                   return <div className="col-md-3">
                     <div className="card mt-5" style={{marginBottom:'40px'}}>
-                      <div className="card-header bg-primary">
+                      <div className="card-header bg-dark">
                         <center>
-                          <img src={product.image} height='150px' alt="No pic" /></center>
+                          <img src={product.image} height='150px' width='180px' alt="No pic" /></center>
                       </div>
                       <div className="card-body text-dark">
                         <li className="list-group-item">Name : {product.name}</li>
